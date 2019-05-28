c699c1f0178c047b45d5b309ebcd53aa12c1453d
k-udupa2000 <karthik.r.udupa@gmail.com>
2019-05-28 22:33:01 +0530

    Error fixed, login front end working

--- Organizer/Registry/views.py
+++ Organizer/Registry/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     #return HttpResponse('Login page')
-    return render(request, reverse_lazy('Registry/login.html'))
+    return render(request, 'Registry/login.html')
